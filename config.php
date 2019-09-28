2db510e8829ac45f71b81e212fa39ba8c1f6da02
Matheus Moura <matheusmfonseca1@gmail.com>
2019-09-28 09:52:04 -0300

    estrutura de pastas

--- config.php
+++ config.php
@@ -2,5 +2,9 @@
 	//Vai ter apenas variaveis e constantes
 	//diretorio raiz
 	define("INCLUDE_PATH","http://localhost/danki/projetoSite/");
+	/*Estrutura de pastas:
+	danki > projetoSite
+	Quando atualizar em outro pc crie uma pasta chamada danki, coloque o git init e clone o repositório
+	*/
 
 ?>
